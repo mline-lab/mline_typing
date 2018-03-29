3b3ba1865630cb028668d647990a938aba01b986
Manaki <manaki079@gmail.com>
2018-03-29 23:48:19 +0900

    sql 接続テスト

--- insert.php
+++ insert.php
@@ -15,4 +15,27 @@
     echo "ミス数:".$miss."</br>";
     echo "名前：".$name;
 
+    try
+    {
+        $pdo = new PDO('mysql:host=localhost;dbname=typing;charset=utf8','mline','manaki079');
+
+        //エラーをスロー
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        $stmt = $pdo -> prepare("INSERT INTO score (name, score, count, miss, mode) VALUES (:name, :score, :count, :miss, :mode)");
+        $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+        $stmt->bindParam(':score', $score, PDO::PARAM_INT);
+        $stmt->bindParam(':count', $count, PDO::PARAM_INT);
+        $stmt->bindParam(':miss', $miss, PDO::PARAM_INT);
+        $stmt->bindValue(':mode', 1, PDO::PARAM_INT);
+
+        $stmt->execute();
+
+        $pdo = null;
+    }
+    catch (PDOException $e)
+    {
+         exit('データベース接続失敗。'.$e->getMessage());
+    }
+
 ?>
