ba5519b0e73d3f293a2ed8fd617769989ba74c20
Manaki <manaki079@gmail.com>
2018-03-31 23:12:18 +0900

    キーかぶってた

--- asset/php/get_session.php
+++ asset/php/get_session.php
@@ -1,8 +1,8 @@
 <?php
     //データの受け取り
-    $score   = $_POST['key_score'];
-    $count   = $_POST['key_downcount'];
-    $miss    = $_POST['key_misscount'];
+    $score   = $_POST['jskey_score'];
+    $count   = $_POST['jskey_downcount'];
+    $miss    = $_POST['jskey_misscount'];
     $dummy_1 = bindec($_POST['yvycc1exry378vzi6k3dsku3nvufc6jzj2tz6ju6l7yyxc60jclhd55mys4xm3uq7pdd60d8hw48mqfkoposgq']);
     $dummy_2 = bindec($_POST['ztb0ubhatdarkngezuhvz8qr66wysyhf9g8qc9vqct1dzwidbl7azx0vqspuo8dyprwlvbqsy49vzjswvsjvh8']);
     $dummy_3 = bindec($_POST['lwuec6xfgpsr3whw3o7pn5dkhsdycm47ytjel6e6hgd7qne3dswthai2epd6y63m5zqe6bcoee04g34mbphp3m']);
